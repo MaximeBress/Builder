0c655f9efc14236a584b41244132bee242f3a6ee
MaximeBress <maxime.bressange@gmail.com>
2017-09-06 08:46:06 +0000

    Fix nodemon for autoload

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Test' });
+    res.render('index', { title: 'Express' });
+});
+router.get('/test', function(req, res, next) {
+    res.render('index', { title: 'Test' });
 });
 
 module.exports = router;
